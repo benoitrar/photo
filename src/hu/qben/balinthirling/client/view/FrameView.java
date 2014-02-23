8e81860465f66a7f5ced6d174ae78c39a02bbd85
Kiss, Benedek <kiss90benedek@gmail.com>
2014-02-23 18:42:40 +0100

    Added stylename to navigating buttons. Refactored a bit, fulfilled javadoc requirements quite rationally. ;)

--- src/hu/qben/balinthirling/client/view/FrameView.java
+++ src/hu/qben/balinthirling/client/view/FrameView.java
@@ -13,18 +13,34 @@ import com.google.gwt.user.client.ui.Image;
  * @author Kiss, Benedek
  * 
  * This class defines the frame's UI.
- * Basically, it has only one element: an image.
+ * The structure is the following:
+ * |---------------------------|
+ * |-----------image-----------|
+ * |---------------------------|
+ * |<5/10> caption-------------|
  */
 public class FrameView extends Composite implements FramePresenter.Display {
+	
+	/**
+	 * Stylenames of the elements.
+	 */
+	private static final String FRAME_TABLE_STYLENAME = "frame-table";
+	private static final String IMAGE_STYLENAME = "img";
+	private static final String CAPTION_LABEL_STYLENAME = "caption-label";
+	private static final String RIGHT_ARROW_STYLENAME = "right-arrow";
+	private static final String RIGHT_BUTTON_STYLENAME = "right-button";
+	private static final String LEFT_ARROW_STYLENAME = "left-arrow";
+	private static final String LEFT_BUTTON_STYLENAME = "left-button";
+	private static final String TEXT_DIV_STYLENAME = "text-div";
 	
 	/**
 	 * The image. Its <code>source</code> will be set by <code>FramePresenter</code>.
 	 */
 	private final Image image = new Image();
 	private final HorizontalPanel infoPanel = new HorizontalPanel();
-	private final Button leftButton = createButton("../img/left_arrow.svg", "left-arrow", "<");
+	private final Button leftButton = createButtonWithIcon("../img/left_arrow.svg", LEFT_ARROW_STYLENAME, "<");
 	private final HTML imageNumberLabel = new HTML("0/0");
-	private final Button rightButton = createButton("../img/right_arrow.svg", "right-arrow", ">");
+	private final Button rightButton = createButtonWithIcon("../img/right_arrow.svg", RIGHT_ARROW_STYLENAME, ">");
 	private final HTML separationPanel = new HTML("&nbsp;&nbsp;");
 	private final HTML captionLabel = new HTML(" ");
 	private final FlexTable content = new FlexTable();
@@ -33,6 +49,18 @@ public class FrameView extends Composite implements FramePresenter.Display {
 	 * Adds an image without functionality.
 	 */
 	public FrameView() {
+		buildUpUI();
+		addStyleNamesToElements();
+	}
+
+	/**
+	 * Builds up the UI. It looks like the following:
+	 * |---------------------------|
+	 * |-----------image-----------|
+	 * |---------------------------|
+	 * |<5/10> caption-------------|
+	 */
+	private void buildUpUI() {
 		initWidget(content);
 		
 		infoPanel.add(leftButton);
@@ -42,44 +70,51 @@ public class FrameView extends Composite implements FramePresenter.Display {
 		infoPanel.add(captionLabel);
 		content.setWidget(0, 0, image);
 		content.setWidget(1, 0, infoPanel);
-		
-		image.setStyleName("img");
-		captionLabel.setStyleName("caption-label");
-		infoPanel.addStyleName("text-div");
-		content.setStyleName("frame-table");
+	}
+	
+	/**
+	 * Adds the proper stylenames to the elements.
+	 * <br />
+	 * Note that every element must have an own stylename.
+	 * By this we can avoid code modifying when content change would be enough.
+	 */
+	private void addStyleNamesToElements() {
+		image.setStyleName(IMAGE_STYLENAME);
+		captionLabel.setStyleName(CAPTION_LABEL_STYLENAME);
+		rightButton.addStyleName(RIGHT_BUTTON_STYLENAME);
+		leftButton.addStyleName(LEFT_BUTTON_STYLENAME);
+		infoPanel.addStyleName(TEXT_DIV_STYLENAME);
+		content.setStyleName(FRAME_TABLE_STYLENAME);
 	}
 
 	/**
-	 * @param path 
-	 * @param styleName
-	 * @param altText
-	 * @return
+	 * @param path the icon's path
+	 * @param styleName the icon's stylename
+	 * @param altText the icon's alt text
+	 * @return button with the specified icon
 	 */
-	private Button createButton(String path, String styleName, String altText) {
+	private Button createButtonWithIcon(String path, String styleName, String altText) {
 		return createButton(createImage(path, styleName, altText));
 	}
 
 	/**
-	 * @param icon
-	 * @return
-	 */
-	private Button createButton(Image icon) {
-		Button button = new Button();
-		button.getElement().appendChild(icon.getElement());
-		return button;
-	}
-	
-	/**
-	 * @param path 
-	 * @param styleName
-	 * @param altText
-	 * @return
+	 * @see FrameView#createButtonWithIcon(String, String, String) createButtonWithIcon
 	 */
 	private Image createImage(String path, String styleName, String altText) {
 		Image image = new Image(path);
 		image.addStyleName(styleName);
 		image.setAltText(altText);
 		return image;
+	}
+	
+	/**
+	 * @param icon that will be placed on the button
+	 * @return button with the given icon
+	 */
+	private Button createButton(Image icon) {
+		Button button = new Button();
+		button.getElement().appendChild(icon.getElement());
+		return button;
 	}
 
 	/* (non-Javadoc)
